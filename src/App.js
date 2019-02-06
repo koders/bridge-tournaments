af9b01c533009492d68bc30951d273d4ad74ecf0
Rihards Fridrihsons <rfridrihsons@gmail.com>
2019-02-06 02:27:15 +0200

    Add facebook login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FacebookLogin from 'react-facebook-login';
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor() {
+        super();
+        this.state = {
+            email: null,
+            name: null,
+            id: null,
+            picture: null,
+        };
+    }
+
+    componentClicked = () => {}
+
+    responseFacebook = (response) => {
+        const {email, id, name, picture} = response;
+        this.setState({
+            email,
+            name,
+            id,
+            picture: picture.data,
+        });
+    }
+
+    render() {
+        const { email, name } = this.state;
+        const loginStuff = email
+            ? <div>
+                <p>Welcome back, {name}</p>
+                <img src={this.state.picture.url} alt="yo" />
+            </div>
+            :
+            <FacebookLogin
+                appId="1126847917389581"
+                autoLoad={true}
+                fields="name,email,picture"
+                onClick={this.componentClicked}
+                callback={this.responseFacebook}
+            />;
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title"> Welcome to React </h1>
+                </header>
+                <div className="App-intro">
+                    {loginStuff}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
